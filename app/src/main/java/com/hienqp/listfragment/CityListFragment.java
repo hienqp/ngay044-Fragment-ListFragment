bead361a76dafb700466115fc849d06655a2c0b9
hienqp <hienphan5290@gmail.com>
2022-09-24 00:12:40 +0700

    add picture show click item

--- app/src/main/java/com/hienqp/listfragment/CityListFragment.java
+++ app/src/main/java/com/hienqp/listfragment/CityListFragment.java
@@ -6,6 +6,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class CityListFragment extends ListFragment {
     String[] cityList = {"Hà Nội","Hải Phòng","Nha Trang","Khánh Hòa","Hồ Chí Minh","Cần Thơ"};
@@ -17,5 +19,12 @@ public class CityListFragment extends ListFragment {
         setListAdapter(adapter);
 
         return inflater.inflate(R.layout.fragment_citylist, container, false);
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        Toast.makeText(getActivity(), cityList[position], Toast.LENGTH_SHORT).show();
+
+        super.onListItemClick(l, v, position, id);
     }
 }
